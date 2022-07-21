48c38526a83c63f98f2fe967d23f6bc4bbbfd64d
michelia <michelia@seal.io>
2022-07-21 17:14:18 +0800

    update dependency

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/aiwantaozi/simple-golang-app
 
 go 1.16
+
+require github.com/sirupsen/logrus v1.9.0
--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"fmt"
+	"github.com/sirupsen/logrus"
 )
 
 func main() {
-	fmt.Println("Hello World")
+	logrus.Info("Hello World")
 }
